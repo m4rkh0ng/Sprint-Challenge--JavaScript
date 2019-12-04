16967810c5d24cc82e83ce1a582d26ad2d150ee4
m4rkh0ng <m4rkh0ng@gmail.com>
2019-12-03 19:00:56 -0500

    functions progressed

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,9 @@
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 
+function consume(a, b, cb){
+  return cb(a, b);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,18 +17,30 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(a, b) {
+  return a + b;
+};
+
+function multiply(a, b) {
+  return a * b;
+};
+
+function greeting(a, b) {
+  return `Hello ${a} ${b}, nice to meet you!}`;
+};
+
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
-// Explain in your own words why nestedfunction can access the variable internal.
+// Explain in your own words why nestedFunction can access the variable internal.
 
-// Explanation: 
+// Explanation: nestedFunction is able to access variable `internal` because the closure groups it in the same scoped environment as each other. It's very much the same way that global variables are accessed from within function scopes that we make without further nesting— the outer function's scope can be accessed from the inner function, kind of like moving from a general to a specific direction. But, the opposite, moving from specific to general would not work (for example, trying to call nestedFunction from outside of myFunction wouldn't work).
 
 
 const external = "I'm outside the function";
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -90,13 +90,17 @@ The resulting contact information strings should have a space between the first 
 "Josh josh@example.com"
 
 Log the result of your new array. */
-const contactInfo = [];
+
+const contactInfo = graduates.map(function(graduate) {
+  return graduate.first_name + ' ' + graduate.email
+});
+
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
-const unisWithUni = [];
-console.log(unisWithUni);
 
+const unisWithUni = graduates.filter(graduate => graduate.university.includes("Uni"));
+console.log(unisWithUni);
 
 // ==== ADVANCED Array Methods ====
 
@@ -120,7 +124,14 @@ const zooAnimals = [
 The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
-const displayNames = [];
+const displayNames = []; 
+
+zooAnimals.forEach(function(animal){
+
+  displayNames.push(animal.animal_name + ' — ' + animal.scientific_name);
+
+});
+
 console.log(displayNames);
 
 /* Request 2: .map()
@@ -129,7 +140,10 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames = [];
+const lowCaseAnimalNames = zooAnimals.map((animal) => {
+  return animal.animal_name.toLowerCase();
+});
+
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
@@ -137,7 +151,8 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals = [];
+
+const lowPopulationAnimals = zooAnimals.filter(animal => animal.population < 5);
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
@@ -145,8 +160,16 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = 0;
-console.log(populationTotal);
+
+let populationTotal = 0;
+const populationValues = zooAnimals.map(animal => {
+  return animal.population
+})
+const reducer = (accumulator, populationTotal) => accumulator + populationTotal;
+
+populationTotal = populationValues.reduce(reducer);
+
+console.log("Zoo's total animal population across the United States:", populationTotal);
 
 
 /*
